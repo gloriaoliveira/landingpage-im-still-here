22a737568b82948d543f147b9c28cc397750e3f5
Gloria Oliveira <gloriost@gmail.com>
2025-03-19 21:53:21 -0300

    setup hero section

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass().on('error', sass.logError))  // Compila o SCSS sem o outputStyle aqui
-        .pipe(cleanCSS({ level: 2 }))  // Minifica o CSS
-        .pipe(gulp.dest('./dist/css'));  // Salva o arquivo minificado
+        .pipe(sass().on('error', sass.logError))  
+        .pipe(cleanCSS({ level: 2 }))  
+        .pipe(gulp.dest('./dist/css'));  
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));  
+}
+
+exports.default = gulp.parallel(styles, images);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss',gulp.parallel(styles));
